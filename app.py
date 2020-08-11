b4064de884a31cca9e73ba1a7c051d5f495e4e1c
PaulyEd <pauledwardswx@gmail.com>
2020-08-11 20:47:13 +0000

    Encountered alot of compatibility issues with ObjectId to JSON in the the user object, have removed ObjectId and replaced with UUID for recipes. login issue related to adding recipe to user with ObjectId no longer and issuer, recipes can be freely saved and dislayed on dashboard on sign in

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from bson.objectid import ObjectId
+# from bson.objectid import ObjectId
 from flask import Flask, render_template, redirect, request, url_for, jsonify, session
 from flask_pymongo import PyMongo
 from passlib.hash import pbkdf2_sha256
@@ -23,6 +23,15 @@ mongo = PyMongo(app)
 app = Flask(__name__)
 app.secret_key = secret_key
 
+# Decorators
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect('/')
+    return wrap
 
 @app.route('/')
 @app.route('/home')
@@ -33,7 +42,8 @@ def show_home():
 
 @app.route('/recipe/<recipe_id>')
 def show_recipe(recipe_id):
-    recipe = mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)})
+    # recipe = mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)})
+    recipe = mongo.db.recipes.find_one({'_id': recipe_id})
     return render_template('recipe.html', recipe=recipe)
 
 
@@ -47,18 +57,64 @@ def signup():
 def signout():
     return User().signout()
 
+
 @app.route('/user/login', methods=['POST'])
 def login():
     return User().login()
 
 
+@app.route('/dashboard/')
+@login_required
+def dashboard():
+    existing_user = mongo.db.users.find_one({'_id': session['username']})
+    saved_recipes = mongo.db.recipes.find({'_id': {'$in': existing_user['recipes']}})
+    return render_template('dashboard.html', saved_recipes=saved_recipes)
+
+
+#login session testing
+@app.route('/test')
+def index():
+    if 'username' in session:
+        existing_user = mongo.db.users.find_one({'_id': session['username']})
+        existing_recipes = jsonify(existing_user["recipes"])
+        saved_recipes = mongo.db.recipes.find({'_id': {'$in': existing_user['recipes']}})
+        # return 'You are logged in as ' + session['username']
+        # return jsonify(session['user'])
+        return existing_recipes
+
+    return 'You are not logged in'
+
+@app.route('/save_recipe/<recipe_id>')
+def save_recipe(recipe_id):
+    updated_recipes = []
+    if session.get('username', None):
+        username = session['username']
+        user = mongo.db.users.find_one({'_id': username})
+        # recipe = mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)})
+        recipe = mongo.db.recipes.find_one({'_id': recipe_id})
+        #  Update users recipes
+        recipes = user['recipes']
+        # if the recipe is not already in the list
+        if recipe_id not in recipes:
+            recipes.append(recipe_id)
+            # recipes.append(ObjectId(recipe_id))
+
+        mongo.db.users.update_one({'_id': user['_id']},
+                            {"$set": {'recipes': recipes, }}, upsert=True)
+
+        updated_user = mongo.db.users.find_one({'_id': username})
+        updated_recipes = updated_user['recipes']
+
+    return redirect(url_for('show_recipe', recipe_id=recipe_id, recipes=updated_recipes))
+
+
 class User:
 
-
-    def start_session(self, user):
+    def start_session(self, user): 
         del user['password']
         session['logged_in'] = True
         session['user'] = user
+        session['username'] = session['user']['_id']
         return jsonify(user), 200
 
     def signup(self):
@@ -73,7 +129,8 @@ class User:
             "_id": uuid.uuid4().hex,
             "name": request.form.get('name'),
             "email": request.form.get('email'),
-            "password": request.form.get('password')
+            "password": request.form.get('password'),
+            "recipes": []
         }
 
         # encrypt
@@ -82,10 +139,12 @@ class User:
         if mongo.db.users.find_one({"email": user['email']}):
             return jsonify({"error": "Email address already in use"}), 400
 
+        # recipe_insert = {"_id":uuid.uuid4().hex,"recipe_name":"cereal 8","recipe_description":"simple morning meal","recipe_ingredients":["cornflakes","milk","sugar"],"prep_time":"5 min","cook_time":"0 min","serves":"1","recipe_steps":["get bowl","add cornflakes","add milk","add sugar"],"notes":"not too much sugar","img_url":"https://res.cloudinary.com/dm2vu1yzr/image/upload/v1596794708/Cornflakes_in_bowl_vmnm7z.jpg","difficulty":"easy"}
+        # mongo.db.recipes.insert_one(recipe_insert)
+
         # add user to DB
         if mongo.db.users.insert_one(user):
             return self.start_session(user)
-            # return jsonify(user), 200
 
         return jsonify({"error": "Signup failed"}), 400
 
@@ -95,29 +154,11 @@ class User:
 
     def login(self):
 
-        user = mongo.db.users.find_one({
-        "email": request.form.get('email')})
+        user = mongo.db.users.find_one({"email": request.form.get('email')})
 
         if user and pbkdf2_sha256.verify(request.form.get('password'), user['password']):
             return self.start_session(user)
         return jsonify({"error": "Invalid login credentials"}), 401
-
-
-# Decorators
-def login_required(f):
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return f(*args, **kwargs)
-        else:
-            return redirect('/')
-    return wrap
-
-
-@app.route('/dashboard/')
-@login_required
-def dashboard():
-    return render_template('dashboard.html')
 
 
 if __name__ == '__main__':
